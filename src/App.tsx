148beeaa08a65f1515d454a829dcae734a86ae41
Alexandra <150593473+Alexandra0994@users.noreply.github.com>
2025-02-01 23:51:59 +0100

    feat: prettier and .env

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 const App = () => {
+  console.log("API URL:", import.meta.env.VITE_API_URL);
+
   return (
-    <div>App</div>
+    <div>API URL: {import.meta.env.VITE_API_URL}</div>
   )
 }
 export default App
---
+++ src/server.ts
@@ -0,0 +1,2 @@
+require('dotenv').config();
+console.log("VITE URL:", import.meta.env.API_URL);
